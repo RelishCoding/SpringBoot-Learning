a9558f4993d28f894b90c5635644336b6ae1db58
qzh <2806894439@qq.com>
2023-06-29 20:58:17 +0800

    20230629SpringBoot案例修复bug：最后一页只有一条数据，删除后最后一页无数据但独立展示

--- springboot_07_ssmp/src/main/java/com/itheima/controller/BookController.java
+++ springboot_07_ssmp/src/main/java/com/itheima/controller/BookController.java
@@ -28,8 +28,8 @@ public class BookController {
         /*R r = new R();
         boolean flag = bookService.save(book);
         r.setFlag(flag);*/
-        if (book.getName().equals("123")) throw new IOException();
         //return new R(bookService.save(book));
+        if (book.getName().equals("123")) throw new IOException();
         boolean flag = bookService.save(book);
         return new R(flag, flag ? "添加成功^_^" : "添加-_-!");
     }
@@ -50,25 +50,18 @@ public class BookController {
     }
 
 
-    /*@GetMapping("{currentPage}/{pageSize}")
+    @GetMapping("{currentPage}/{pageSize}")
     public R getPage(@PathVariable int currentPage,@PathVariable int pageSize){
         IPage<Book> page = new Page<>(currentPage,pageSize);
+        bookService.page(page);
+
         //如果当前页码值大于总页码值，则重新执行查询操作，使用最大页码值作为当前页码值
         if (currentPage > page.getPages()){
             page = new Page<>(page.getPages(),pageSize);
+            bookService.page(page);
         }
-        return new R(true,bookService.page(page));
-    }*/
 
-    @GetMapping("{currentPage}/{pageSize}")
-    public R getPage(@PathVariable int currentPage,@PathVariable int pageSize,Book book){
-        //System.out.println("参数==》"+book);
-
-        IPage<Book> page = new Page<>(currentPage,pageSize);
-        //如果当前页码值大于总页码值，则重新执行查询操作，使用最大页码值作为当前页码值
-        if (currentPage > page.getPages()){
-            page = new Page<>(page.getPages(),pageSize);
-        }
-        return new R(true,bookService.page(page));
+        //return new R(true,bookService.page(page));
+        return new R(true,page);
     }
 }
