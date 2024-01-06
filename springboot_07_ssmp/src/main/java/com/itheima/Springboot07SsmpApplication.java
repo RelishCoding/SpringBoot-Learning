3b5138365ca83f0adda8257a62269e8a274ec720
qzh <2806894439@qq.com>
2024-01-06 22:21:00 +0800

    开发环境中使用临时属性

--- springboot_07_ssmp/src/main/java/com/itheima/Springboot07SsmpApplication.java
+++ springboot_07_ssmp/src/main/java/com/itheima/Springboot07SsmpApplication.java
@@ -3,11 +3,22 @@ package com.itheima;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class Springboot07SsmpApplication {
 
     public static void main(String[] args) {
+        //System.out.println(Arrays.toString(args));
+
+        /*String[] arg = new String[1];
+        arg[0] = "--server.p[ort=8082";
+        SpringApplication.run(Springboot07SsmpApplication.class, arg);*/
+
         SpringApplication.run(Springboot07SsmpApplication.class, args);
+
+        //可以在启动boot程序时断开读取外部临时配置的入口，也就是去掉读取外部参数的形参
+        //SpringApplication.run(Springboot07SsmpApplication.class);
     }
 
 }
