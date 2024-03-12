20e526bc56970b7e620ee1524731521aeb21d2fb
qzh <2806894439@qq.com>
2024-03-12 21:45:22 +0800

    application

---
+++ springboot_10_log/src/main/java/com/itheima/Springboot10LogApplication.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot10LogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot10LogApplication.class, args);
+    }
+}
